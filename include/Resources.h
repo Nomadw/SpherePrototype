495d08035096bcb1f4857b40c496357646e8b778
Nomadw <damon_wareham@yahoo.com>
2015-10-26 09:53:55 +0000

    Create Resources.h

---
+++ include/Resources.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "cinder/CinderResources.h"
+
+//#define RES_MY_RES			CINDER_RESOURCE( ../resources/, image_name.png, 128, IMAGE )
+
+#define RES_SHADER_FRAG		CINDER_RESOURCE( ../resources/, phong_frag.glsl, 128, GLSL )
+#define RES_SHADER_VERT		CINDER_RESOURCE( ../resources/, phong_vert.glsl, 129, GLSL )
+
+#define RES_TEXTURE_1		CINDER_RESOURCE( ../resources/, fire.jpg, 130, IMAGE )
+#define RES_TEXTURE_2		CINDER_RESOURCE(../resources/, greenfire.jpg, 131, IMAGE )
+#define RES_TEXTURE_3		CINDER_RESOURCE(../resources/, particle.jpg, 132, IMAGE )
+
