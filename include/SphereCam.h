ae8a7994df399f49de5e3578cbaba31c16ad5edb
Nomadw <damon_wareham@yahoo.com>
2016-01-14 19:04:21 +0000

    Update SphereCam.h

--- include/SphereCam.h
+++ include/SphereCam.h
@@ -2,17 +2,21 @@
 
 #include "Globals.h"
 
-//Code taken from cinder/MayaCam - Created for any edits to camera usage
+//Code taken and edited from cinder/MayaCam - Created for any edits to camera usage
+
+//Wheel zoom functions created, no longer used but left (commented) in
+
+//Controls for camera were altered for Mac usage in mind (Can all be done with one mouse / trackpad button) - Also works fine with Windows!
 
 class SphereCam {
 private:
 	enum		{ ACTION_NONE, ACTION_ZOOM, ACTION_PAN, ACTION_TUMBLE, ACTION_ZOOM2 };
 
 	Vec2i		mInitialMousePos;
-	float		mInitialMouseWheel;
-	CameraPersp	mCurrentCam, mInitialCam;
+	//float		mInitialMouseWheel;
+	CameraPersp	mCurrentCam, mInitialCam; //Uses camera perspectives from Cinder's camera.h
 	int			mLastAction;
-	float		mLastWheelAction;
+	//float		mLastWheelAction;
 
 public:
 	SphereCam(); 
@@ -20,8 +24,9 @@ public:
 
 	void mouseDown(const Vec2i &mousePos);
 	void mouseDrag(const Vec2i &mousePos, bool rightDown, bool controlDown, bool leftDown, bool shiftDown);
-	void wheelZoom(float wheelIncrement);
+	//void wheelZoom(float wheelIncrement);
 
+	//Returns position
 	Vec3f GetPosition() { return mCurrentCam.getEyePoint(); }
 
 	const CameraPersp& getCamera() const { return mCurrentCam; }
